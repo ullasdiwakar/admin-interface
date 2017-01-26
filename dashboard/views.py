bbfeb98f33774923752b788460f11c602497e3fe
Ullas Diwakar <ullasdiwakar96@gmail.com>
2017-01-27 01:59:58 +0530

    First render

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("You are in the Dashboard.")
+    return render(request, "index.html")
