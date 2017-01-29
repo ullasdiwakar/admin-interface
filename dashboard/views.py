b4eff9d07cd3dc99105f13c4a481c282fe2f5a05
Ullas Diwakar <ullasdiwakar96@gmail.com>
2017-01-30 02:36:07 +0530

    Created Login interface

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-def index(request):
-    return render(request, "index.html")
+@login_required(login_url = "login/")
+def home(request):
+    return render(request,"home.html")
